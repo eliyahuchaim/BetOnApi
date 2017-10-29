39de7ff5ff01d4360ee0f735d5af8b83fcd5c231
github username <github email address>
2017-10-29 15:08:55 -0400

    wagers and bets update users score

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -1,4 +1,38 @@
 class Bet < ApplicationRecord
   belongs_to :party
   has_many :wagers
+
+
+  def self.create_bet payload
+    start_d = Date.parse(payload["start_date"])
+    end_d = Date.parse(payload["end_date"])
+    @bet = Bet.create(bet_type: payload["bet_type"], value: payload["value"], ended: false, party_id: payload["party_id"], start_date: start_d, end_date: end_d)
+    if @bet
+      @bet
+    else
+      @bet.errors.full_messeges
+    end
+  end
+
+  def self.find_ended_bets
+    today = Time.now.to_date
+    due_bets = []
+    for bet in Bet.all
+      bet.end_date <= today ? due_bets << bet : nil
+    end
+    self.find_wagers due_bets
+  end
+
+  def self.find_wagers bets
+    bets.each do |b|
+      bet = Bet.find(b.id)
+      wagers = bet.wagers
+      User.update_user_points(wagers, bet.result, bet.value)
+    end
+  end
+
+
+
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,14 @@ class User < ApplicationRecord
     UserQueries.find_friends(self.id)
   end
 
+  def self.update_user_points(wagers, bet_result, bet_value)
+    wagers.each do |w|
+      user = User.find(w.user_id)
+      w.bet_answer === bet_result ? user.increment!("points", by = bet_value) : user.decrement!("points", by = bet_value)
+      user.save!
+    end
+  end
+
+
+
 end
