a6c59b1efc9163c4b1ad6b79e8870f251b005a55
github username <github email address>
2017-11-09 16:58:07 -0500

    group validation

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,9 +3,6 @@ class Group < ApplicationRecord
   belongs_to :user
   validate :validate_user_not_in_group
 
-
-  # TODO: add validation for unique members of a group
-
   def validate_user_not_in_group
     result = UserQueries.check_if_group_has_user party_id, user_id
     result ? errors.add(:user_id, "user is already in this party") : true
