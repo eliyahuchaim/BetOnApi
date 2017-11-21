12fda0508f0d5151cd39c839e786d854df82a254
github username <github email address>
2017-11-21 15:48:22 -0500

    added avatar params and fixed cors issue

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -38,7 +38,7 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:firstname, :lastname, :username, :password)
+    params.require(:user).permit(:firstname, :lastname, :username, :password, :avatar)
   end
 
   def give_points_params
