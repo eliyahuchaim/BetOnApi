433cddc7ec6ee8542dc23098047ad3f154cd202d
github username <github email address>
2017-11-21 12:56:24 -0500

    added avatar column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171110000747) do
+ActiveRecord::Schema.define(version: 20171121174730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20171110000747) do
     t.integer "points", default: 100
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
   end
 
   create_table "wagers", force: :cascade do |t|
