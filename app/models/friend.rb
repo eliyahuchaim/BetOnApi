9132017a45225c04e7050c0f346b042bbc620761
github username <github email address>
2017-11-27 17:48:45 -0500

    no duplicate friends validator

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,5 +1,16 @@
 class Friend < ApplicationRecord
+  validate :non_duplicates, :on => :create
 
-  # validate non duplicates
 
+  def non_duplicates
+    for friend in Friend.all
+      errors.add(:friend, "users are already friends") if check_if_friends_exist friend, user1, user2
+      end
+    end
+  end
+
+  def check_if_friends_exist friend, user1, user2
+    if friend.user1 == user1 && friend.user2 == user2 || friend.user1 == user2 && friend.user2 == user1
+    true
+  end
 end
